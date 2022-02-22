c2bbe671b1ea784c61810266670cba58743e8056
fadelananda <fadelananda123@gmail.com>
2022-02-22 23:05:26 +0700

    add draw function and get pixel coordinates from mousedown

--- app/app.js
+++ app/app.js
@@ -2,11 +2,14 @@ import {
   setUpCanvasBackground,
   createShader,
   createProgram,
+  draw,
+  getCoordinate,
 } from "./util/util.js";
 
 function main() {
   const canvas = document.querySelector("canvas");
   const gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
+  const points = [];
 
   if (!gl) return;
 
@@ -24,58 +27,23 @@ function main() {
   // Link the two shaders into a program
   var program = createProgram(gl, vertexShader, fragmentShader);
 
-  // look up where the vertex data needs to go.
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
-  // Create a buffer and put three 2d clip space points in it
-  var positionBuffer = gl.createBuffer();
-
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  var positions = [0, 0, 0, 0.5, 0.7, 0];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  // code above this line is initialization code.
-  // code below this line is rendering code.
-
-  // webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  // Clear the canvas
-  setUpCanvasBackground(gl);
-
-  // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
 
-  // Turn on the attribute
-  gl.enableVertexAttribArray(positionAttributeLocation);
+  var vertices = [
+    -0.5, -0.5, -0.5, +0.5, 0.0, +0.5, 0.0, 0.0, 0.0, -0.5, +0.5, -0.5,
+  ];
 
-  // Bind the position buffer.
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  setUpCanvasBackground(gl);
+  draw(gl, vertices, program, gl.TRIANGLES);
 
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2; // 2 components per iteration
-  var type = gl.FLOAT; // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0; // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-    positionAttributeLocation,
-    size,
-    type,
-    normalize,
-    stride,
-    offset
-  );
-
-  // draw
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 3;
-  gl.drawArrays(primitiveType, offset, count);
+  canvas.addEventListener("mousedown", (event) => {
+    // console.log(getCoordinate(event, [], canvas));
+    // points.push(getCoordinate(event, points, canvas));
+    // var point = getCoordinate(event, points, canvas)
+    points.push(getCoordinate(event, points, canvas).x);
+    points.push(getCoordinate(event, points, canvas).y);
+    draw(gl, points, program, gl.TRIANGLES);
+  });
 }
 
 main();
--- app/util/util.js
+++ app/util/util.js
@@ -2,7 +2,7 @@ const setUpCanvasBackground = (gl) => {
   /**
    * Set canvas color to some rgba value
    */
-  gl.clearColor(0, 0, 0, 0);
+  gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 };
 
@@ -37,4 +37,58 @@ const createProgram = (gl, vertexShader, fragmentShader) => {
   gl.deleteProgram(program);
 };
 
-export { setUpCanvasBackground, createProgram, createShader };
+const createGlBuffer = (gl, array, program) => {
+  /**
+   * Create buffer and get attribute
+   */
+  var vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) throw new Error("failed to create buffer");
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(array), gl.STATIC_DRAW);
+
+  var aPosition = gl.getAttribLocation(program, "a_position");
+  if (aPosition < 0) throw new Error("failed to get attribute from program");
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2;
+  var type = gl.FLOAT; // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0; // start at the beginning of the buffer
+  gl.vertexAttribPointer(
+    aPosition,
+    size,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+
+  gl.enableVertexAttribArray(aPosition);
+
+  return array.length/size;
+};
+
+const draw = (gl, array, program, type) => {
+  var n = createGlBuffer(gl, array, program);
+  if (n<0) throw new Error('failed to initialize buffer');
+
+  
+  // gl.drawArrays(gl.LINE_STRIP, 0, n);
+  // gl.drawArrays(gl.LINE_LOOP, 0, n);
+  gl.drawArrays(type, 0, n);
+  // gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+};
+
+const getCoordinate = (event, points, canvas) => {
+  var x = event.clientX, y = event.clientY;
+
+  var midX = canvas.width/2, midY = canvas.height/2;
+
+  var rect = event.target.getBoundingClientRect();
+
+  // return [((x - rect.left) - midX)/midX, (midY - (y - rect.top))/midY];
+  return {x:((x - rect.left) - midX)/midX, y:(midY - (y - rect.top))/midY};
+}
+
+export { setUpCanvasBackground, createProgram, createShader, draw, getCoordinate };
